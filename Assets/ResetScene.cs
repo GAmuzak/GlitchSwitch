8e0eef34cc427efebc0d81322fdb2d6f16c19478
omangbaheti <60500898+omangbaheti@users.noreply.github.com>
2021-10-23 22:07:04 +0530

    Swapping Mechanic Works

---
+++ Assets/ResetScene.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    [SerializeField] private GameObject glitchedBody;
+    private static ResetScene _instance;
+    public static Vector3 oldPlayerPosition;
+    private bool teleport1 = true;
+    public ResetScene Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            glitchedBody = Instantiate(glitchedBody, oldPlayerPosition, Quaternion.identity);
+            glitchedBody.tag = "GlitchedBody";
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnReload();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+           SwapPositions();
+        }
+    }
+    
+    
+    public void OnReload()
+    {
+        CharacterController player = FindObjectOfType<CharacterController>();
+        oldPlayerPosition = player.transform.position;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+    
+    private void SwapPositions()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        CharacterController playerController = player.GetComponent<CharacterController>();
+        Transform glitchBody = GameObject.FindWithTag("GlitchedBody").transform;
+        playerController.enabled = false;
+        //Debug.Log($"Player:{player.position} GlitchedBody{glitchBody.position}");
+        (player.transform.position, glitchBody.position) = (glitchBody.position, player.transform.position);
+        playerController.enabled = true;
+        //Debug.Log($"After swapping Player:{player.position} GlitchedBody{glitchBody.position}");
+    }
+}
