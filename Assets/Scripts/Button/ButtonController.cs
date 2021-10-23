d6a3810f5b250f7a33ed7f93795a1919e4f6a841
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-10-24 00:49:04 +0530

    where my meshes at

--- Assets/Scripts/Button/ButtonController.cs
+++ Assets/Scripts/Button/ButtonController.cs
@@ -5,9 +5,17 @@ public class ButtonController : MonoBehaviour
 {
     [SerializeField] private List<GateController> gates;
 
+    private Animator buttonAnimator;
+
+    private void Start()
+    {
+        buttonAnimator = GetComponent<Animator>();        
+    }
+
     private void OnMouseDown()
     {
         OnInteract();
+        buttonAnimator.Play("ButtonPress", -1, 0f);
     }
 
     public void OnInteract()
