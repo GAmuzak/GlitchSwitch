d2c8bc52cb0925abe08f0f8332b0916b0653bd74
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2021-10-23 23:07:43 +0530

    Sci Fi Gate Animation

--- Assets/Scripts/Button/GateController.cs
+++ Assets/Scripts/Button/GateController.cs
@@ -2,14 +2,18 @@
 
 public class GateController : MonoBehaviour
 {
-    [SerializeField] private Transform[] locations;
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
+    [SerializeField] private Transform[] locationsDoorLeft;
+    [SerializeField] private Transform[] locationsDoorRight;
     [SerializeField] private float time;
 
     [SerializeField] private int state = 0;
 
     public void FlipState()
     {
-        LeanTween.move(gameObject, locations[state].position, time * Time.deltaTime / 10f);
+        LeanTween.move(leftDoor, locationsDoorLeft[state].position, time * Time.deltaTime / 10f);
+        LeanTween.move(rightDoor, locationsDoorRight[state].position, time * Time.deltaTime / 10f);
         state = 1 - state;
     }
 }
