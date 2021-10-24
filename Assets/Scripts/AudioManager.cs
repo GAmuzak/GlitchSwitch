c09f10d7ab3d8b3692a44f0090132327fdcc2e8b
GAmuzak <thatamuzak@gmail.com>
2021-10-24 16:31:14 +0530

    AudioManagerFinishes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,26 +6,39 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private List<AudioClip> type;
+    private static List<AudioClip> type=new List<AudioClip>();
 
     private static AudioSource _audioSource;
 
     private void Start()
     {
+        type.Add(Resources.Load<AudioClip>("AudioClips/DoorOpen"));
+        type.Add(Resources.Load<AudioClip>("AudioClips/DoorClose"));
+        type.Add(Resources.Load<AudioClip>("AudioClips/ButtonPress"));
         _audioSource = GetComponent<AudioSource>();
     }
 
-    public static void PlaySound()
+    public static void PlaySound(Sound sound)
     {
-        
+        switch (sound)
+        {
+            case Sound.DoorOpen:
+                _audioSource.PlayOneShot(type[0]);
+                break;
+            case Sound.DoorClose:
+                _audioSource.PlayOneShot(type[1]);
+                break;
+            case Sound.ButtonPress:
+                _audioSource.PlayOneShot(type[2]);
+                break;
+        }
     }
 }
 
-public enum Sounds
+public enum Sound
 {
     NULL=-1,
-    Footsteps=0,
-    DoorOpen=1,
-    DoorClose=2,
-    ButtonPress=3
+    DoorOpen=0,
+    DoorClose=1,
+    ButtonPress=2
 }
